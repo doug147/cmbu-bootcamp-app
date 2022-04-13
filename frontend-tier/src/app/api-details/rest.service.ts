cb797f3c8fb1a7bace6217713fed05c143788ac7
doug147 <douglorenzen@gmail.com>
2022-04-13 09:24:51 -0400

    Update rest.service.ts

--- frontend-tier/src/app/api-details/rest.service.ts
+++ frontend-tier/src/app/api-details/rest.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
-const endpoint = 'https://lab-vra.meteialab.com'
+const endpoint = 'http://'+window.location.host+'/api'
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
